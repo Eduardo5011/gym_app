d6d54b857ed51a225323794717ea097d6818a95a
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-06-22 10:50:35 -0400

    update

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -36,7 +36,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     }
 
     fetchExercisesData();
-  }, [bodyPart])
+  }, [bodyPart, setExercises])
 
   return (
     <Box id="exercises"
