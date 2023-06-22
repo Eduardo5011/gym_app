0cac0909aac8427371d63b4f1dcd9eaf36183b19
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-06-22 15:27:12 -0400

    update

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,28 +1,23 @@
 export const exerciseOptions = {
-  method: 'GET',
-  
+  method: "GET",
+
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
 };
 
-
-
-
 export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
 
-    const response = await fetch(url, options);
-    const data = await response.json();
-
-    return data;
+  return data;
 };
 
 export const youtubeOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
 };
-
